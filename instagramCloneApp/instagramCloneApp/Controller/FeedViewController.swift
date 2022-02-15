4ea715990d8494390a2ad24ee8cf4e11c236ae64
muhammad.ashraf <muhammad.ashraf@qburst.com>
2022-02-15 23:25:44 +0530

    Created feeds collection view cell

--- instagramCloneApp/instagramCloneApp/Controller/FeedViewController.swift
+++ instagramCloneApp/instagramCloneApp/Controller/FeedViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 
-class FeedViewController: UIViewController{
+class FeedViewController: UIViewController, UICollectionViewDataSource{
     
-//    MARK: - Lifecycle
+    let feedsCollectionView: UICollectionView = {
+        let viewLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    private let reuseIdentifier = "cell"
+    
+//  MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCpllectionView()
+    }
+    
+//  MARK: - Helper
+    
+    func setupCpllectionView(){
+        self.view.addSubview(feedsCollectionView)
+        feedsCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            feedsCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            feedsCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            feedsCollectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            feedsCollectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+        feedsCollectionView.dataSource = self
+        feedsCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+//  MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
     
 }
